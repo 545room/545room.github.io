6b541ad0343097c0546fe5d68dca92c10f41b5b8
545room <n88dfull@gmail.com>
2023-12-28 17:06:04 +0300

    5

--- dentist/js/main.js
+++ dentist/js/main.js
@@ -26,6 +26,8 @@ document.addEventListener('click', e => {
 
 jQuery('.marquee').marquee({
   duration: 30000,
+  startVisible: true,
+  duplicated: true
 });
 
 $(function(){
@@ -34,13 +36,13 @@ $(".twentytwenty").twentytwenty({
 });
 });
 
-$(window).load(function() {
   $('.slider').slick({
     centerPadding: '220px',
     centerMode: true,
     slidesToShow: 1,
     draggable: false,
     swipe: false,
+    adaptiveHeight: true,
     prevArrow: '<div class="bafter-arrow_left"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
     nextArrow: '<div class="bafter-arrow_right"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
     responsive: [
@@ -78,7 +80,6 @@ $(window).load(function() {
     ]
     });
 
-});
 
 
 
