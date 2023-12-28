3a07ec7dfb78d3ab169ae0f79a9f2fbe54d82409
545room <n88dfull@gmail.com>
2023-12-28 16:04:49 +0300

    4
    slick slider has been made ready

--- dentist/js/main.js
+++ dentist/js/main.js
@@ -35,51 +35,6 @@ $(".twentytwenty").twentytwenty({
 });
 
 
-
-$('.slider').slick({
-centerPadding: '220px',
-centerMode: true,
-slidesToShow: 1,
-draggable: false,
-swipe: false,
-prevArrow: '<div class="bafter-arrow_left"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
-nextArrow: '<div class="bafter-arrow_right"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
-responsive: [
-  {
-    breakpoint: 1768,
-    settings: {
-      centerPadding: '150px',
-    }
-  },
-  {
-    breakpoint: 1495,
-    settings: {
-      centerPadding: '80px',
-    }
-  },
-  {
-    breakpoint: 1366,
-    settings: {
-      centerPadding: '30px',
-    }
-  },
-  {
-    breakpoint: 1277,
-    settings: {
-      centerPadding: '10px',
-    }
-  },
-  {
-    breakpoint: 1200,
-    settings: {
-      centerPadding: '0px',
-      centerMode: false,
-    }
-  }
-]
-});
-
-
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
 anchor.addEventListener('click', function (e) {
@@ -128,5 +83,53 @@ $(document).ready(function(){
     }
     return false;
   });
+
+
+
+  $('.slider').slick({
+    centerPadding: '220px',
+    centerMode: true,
+    slidesToShow: 1,
+    draggable: false,
+    swipe: false,
+    prevArrow: '<div class="bafter-arrow_left"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
+    nextArrow: '<div class="bafter-arrow_right"><svg width="17" height="26" viewBox="0 0 17 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.0015 25.0424C16.6662 24.4322 16.6662 23.4428 16.0015 22.8326L5.26584 13L16.0015 3.16736C16.6662 2.55716 16.6662 1.56784 16.0015 0.957647C15.3367 0.347452 14.259 0.347452 13.5943 0.957647L0.499999 13L13.5943 25.0424C14.259 25.6525 15.3367 25.6525 16.0015 25.0424Z" fill="#DEB359"/></svg></div>',
+    responsive: [
+      {
+        breakpoint: 1768,
+        settings: {
+          centerPadding: '150px',
+        }
+      },
+      {
+        breakpoint: 1495,
+        settings: {
+          centerPadding: '80px',
+        }
+      },
+      {
+        breakpoint: 1366,
+        settings: {
+          centerPadding: '30px',
+        }
+      },
+      {
+        breakpoint: 1277,
+        settings: {
+          centerPadding: '10px',
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          centerPadding: '0px',
+          centerMode: false,
+        }
+      }
+    ]
+    });
+
+
+
 });
 
